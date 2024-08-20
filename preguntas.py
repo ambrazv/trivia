dac266c41d2e6b4abaf5ebb6238d2f00b843ea3d
Ambar Zambrano Venegas <ambaar.venegas@gmail.com>
2024-08-19 22:59:00 -0400

    Subiendo Archivo preguntas.py

---
+++ preguntas.py
@@ -0,0 +1,61 @@
+preguntas_basicas = {
+    'pregunta_1': {'enunciado':['¿En qué año comenzó la Guerra de Independencia de Chile?'],
+    'alternativas': [['1820', 0], 
+                     ['1810', 1], 
+                     ['1800', 0], 
+                     ['1810', 0]]},
+    'pregunta_2': {'enunciado':['¿En qué año rescataron a los 33 mineros de la mina San José?'],
+    'alternativas': [['2012', 0], 
+                     ['2010', 1], 
+                     ['2008', 0], 
+                     ['2007', 0]]},
+    
+'pregunta_3': {'enunciado':['¿Quién ganó el premio Nobel de Literatura en 1945?'],
+    'alternativas': [['Pablo Neruda', 0], 
+                     ['Gabriela Mistral', 1], 
+                     ['Nicanor Parra', 0], 
+                     ['Isabel Allende', 0]]}
+}
+
+preguntas_intermedias = {
+    'pregunta_1': {'enunciado':['¿Qué deportista chileno ganó la medalla de oro en tenis en los Juegos Olímpicos de Atenas 2004?'],
+    'alternativas': [['Fernando Gonzalez', 0], 
+                     ['Nicolás Massú', 1], 
+                     ['Marcelo Rios', 0], 
+                     ['Alejandro Tabilo', 0]]},
+    'pregunta_2': {'enunciado':['¿Quién compuso e interpretó la canción "Gracias a la vida"?'],
+    'alternativas': [['Francisca Valenzuela', 0], 
+                     ['Violeta Parra', 1], 
+                     ['Mon Laferte', 0], 
+                     ['Yuri', 0]]},
+    
+'pregunta_3': {'enunciado':['¿De qué ciudad proviene la agrupación chilena Los Bunkers?'],
+    'alternativas': [['Valdivia', 0], 
+                     ['Concepción', 1], 
+                     ['Temuco', 0], 
+                     ['Coquimbo', 0]]}
+}
+
+preguntas_avanzadas = {
+    'pregunta_1': {'enunciado':['¿Qué se celebra el 8 de diciembre en Chile?'],
+    'alternativas': [['La Asunción de la Virgen de Lourdes', 0], 
+                     ['La Inmaculada Concepción', 1], 
+                     ['Santa Teresa de los Andes', 0], 
+                     ['La Asunción de la Virgen María', 0]]},
+    'pregunta_2': {'enunciado':['Ciudad chilena que limita con Tacna, Perú '],
+    'alternativas': [['Iquique', 0], 
+                     ['Arica', 1], 
+                     ['Antofagasta', 0], 
+                     ['Calama', 0]]},
+    
+'pregunta_3': {'enunciado':['Nombre del principal puerto de Chile'],
+    'alternativas': [['San Antonio', 0], 
+                     ['Valparaíso ', 1], 
+                     ['Valdivia', 0], 
+                     ['Calama', 0]]}
+}
+
+pool_preguntas = {'basicas': preguntas_basicas,
+                  'intermedias': preguntas_intermedias,
+                  'avanzadas': preguntas_avanzadas}
+
