ec7a1c2844b21a4346d3e218851ce96ad870c789
Karen Limari <karenlimari20@gmail.com>
2024-08-21 04:48:50 +0200

    update print_preguntas

--- print_preguntas.py
+++ print_preguntas.py
@@ -1,5 +1,6 @@
 import preguntas as p
 
+
 def print_pregunta(enunciado, alternativas):
     """
     Imprime una pregunta con su enunciado y sus alternativas numeradas.
@@ -16,14 +17,14 @@ def print_pregunta(enunciado, alternativas):
     print()  # Salto de línea
 
     # Imprimir cada alternativa con su letra asociada
-    letras = ['A', 'B', 'C', 'D']
+    letras = ["A", "B", "C", "D"]
     for i, alternativa in enumerate(alternativas):
-        print(f"{letras[i]}. {alternativa}")
+        print(f"{letras[i]}. {alternativa[0]}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     # Obtener la pregunta del pool de preguntas
-    pregunta = p.pool_preguntas['basicas']['pregunta_1']
-    
-    # Imprimir la pregunta y sus alternativas
-    print_pregunta(pregunta['enunciado'], pregunta['alternativas'])
+    pregunta = p.pool_preguntas["basicas"]["pregunta_1"]
 
+    # Imprimir la pregunta y sus alternativas
+    print_pregunta(pregunta["enunciado"], pregunta["alternativas"])
